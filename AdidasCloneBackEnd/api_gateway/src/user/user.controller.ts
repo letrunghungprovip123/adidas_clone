3866234fbe404452bb3bdfb4b428ac4e4666b3c8
Nam <thecallingspectre@gmail.com>
2025-11-30 14:15:29 +0700

    optimal @Put('updateUser/:id')

--- AdidasCloneBackEnd/api_gateway/src/user/user.controller.ts
+++ AdidasCloneBackEnd/api_gateway/src/user/user.controller.ts
@@ -87,29 +87,26 @@ export class UserController {
     @Param('id', ParseIntPipe) id: number,
   ) {
     try {
-      const result = await lastValueFrom(
+      return await lastValueFrom(
         this.userServiceClient.send(
           { cmd: RMQ_PATTERN_USER.UPDATE_USER },
-          { user, id },
-        ),
+          { user, id }
+        )
       );
-      return result;
-    } catch (error) {
-      if (error && typeof error === 'object' && error.statusCode) {
-        switch (error.statusCode) {
-          case 400:
-            throw new BadRequestException(error.message);
-          case 404:
-            throw new NotFoundException(error.message);
-          // các trường hợp khác nếu cần
-          default:
-            throw new InternalServerErrorException(error.message);
-        }
+    } catch (error: any) {
+      const map = {
+        400: BadRequestException,
+        404: NotFoundException,
+      };
+
+      if (error?.statusCode && map[error.statusCode]) {
+        throw new map[error.statusCode](error.message);
       }
 
       throw new InternalServerErrorException('User service failed');
     }
   }
+
 
   @Delete('deleteUser/:id')
   async deleteUser(@Param('id', ParseIntPipe) id: number) {
